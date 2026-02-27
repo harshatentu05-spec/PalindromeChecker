2d2062659bf75ca4f498bf1b85afc0aed2ba547b
harshatentu05 <harshatentu05@gmail.com>
2026-02-27 15:16:04 +0530

    Harsha added UC3

--- App/src/PalindromeChecker.java
+++ App/src/PalindromeChecker.java
@@ -2,14 +2,14 @@ public class PalindromeChecker {
 
     public static void main(String[] args) {
 
-        String word = "madam";
+        String word = "racecar";
         String reversed = "";
 
-        for(int i = word.length() - 1; i >= 0; i--) {
+        for (int i = word.length() - 1; i >= 0; i--) {
             reversed = reversed + word.charAt(i);
         }
 
-        if(word.equals(reversed)) {
+        if (word.equals(reversed)) {
             System.out.println(word + " is a Palindrome.");
         } else {
             System.out.println(word + " is not a Palindrome.");
