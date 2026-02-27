6f4b524ee2f15b5088720824c78e528e8f0af11a
harshatentu05 <harshatentu05@gmail.com>
2026-02-27 14:58:12 +0530

    Harsha added UC1

--- App/src/PalindromeChecker.java
+++ App/src/PalindromeChecker.java
@@ -1,5 +1,5 @@
 public class PalindromeChecker {
     public static void main(String[] args){
-        System.out.println("Welcome to PalindromeChecker App");
+        System.out.println("Welcome to The PalindromeChecker App");
     }
 }
